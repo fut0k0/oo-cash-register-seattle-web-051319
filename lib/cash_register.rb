d2bbe7031dbcff96cd60de9131ddd2a11f3eb2a4
fut0k0 <cccabell@icloud.com>
2019-05-16 17:42:21 -0700

    Passing lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
-
+class CashRegister
+	attr_accessor :total
+	attr_reader :discount, :items
+		
+	def initialize(discount=0)
+		@total = 0
+		@discount = discount
+		@items = []
+	end
+	
+	def add_item(title, price, quantity=1)
+		@previous = price * quantity
+		@total += price * quantity
+		while quantity > 0
+			@items.push(title)
+			quantity -= 1
+		end
+	end
+	
+	def apply_discount
+		@total = @total * (1 - @discount.to_f / 100)
+		if @discount == 0
+			"There is no discount to apply."
+		else
+			"After the discount, the total comes to $#{@total.to_i}."
+		end
+	end
+	
+	def void_last_transaction
+		@total -= @previous
+	end
+end
